4370be14bf34d34db3edfe309ce4dade4029d619
unknown <jubrilmuritala69@gmail.com>
2023-08-11 15:46:43 +0100

    carousel

--- src/app/Components/carousel/carousel.component.ts
+++ src/app/Components/carousel/carousel.component.ts
@@ -1,17 +1,55 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+interface Logos {
+  image: string;
+  caption: string;
+}
+const client_logos: Logos[] = [
+  {
+    image: '/assets/images/clientlogo.jpg',
+    caption: '',
+  },
+  {
+    image: '/assets/images/clientlogo1.jpg',
+    caption: '',
+  },
+  {
+    image: '/assets/images/clientlogo2.jpg',
+    caption: '',
+  },
+  {
+    image: '/assets/images/clientlogo3.jpg',
+    caption: '',
+  },
+  {
+    image: '/assets/images/clientlogo4.jpg',
+    caption: '',
+  },
+  {
+    image: '/assets/images/clientlogo5.jpg',
+    caption: '',
+  },
+  {
+    image: '/assets/images/clientlogo6.jpg',
+    caption: '',
+  },
+  {
+    image: '/assets/images/clientlogo7.jpg',
+    caption: '',
+  },
+];
+
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
 })
 export class CarouselComponent implements OnInit {
   @Input() img: any;
   @Input() sub!: string;
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
-  constructor() { }
+  constructor() {}
+  clientlogos = client_logos;
   active: boolean = false;
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
