5a16b85e71758e40803744c51a07b925422fb1d5
unknown <jubrilmuritala69@gmail.com>
2023-08-28 10:30:06 +0100

    onboarding

--- src/app/Pages/vendoronboarding/vendoronboarding.component.ts
+++ src/app/Pages/vendoronboarding/vendoronboarding.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-vendoronboarding',
   templateUrl: './vendoronboarding.component.html',
-  styleUrls: ['./vendoronboarding.component.css']
+  styleUrls: ['./vendoronboarding.component.css'],
 })
 export class VendoronboardingComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  rollsafe() {
+    window.open('https://partner.halobizapps.com/#/', '_blank');
   }
-
+  learnmore() {
+    window.open('https://partner.halobizapps.com/#/learn-more', '_blank');
+  }
 }
