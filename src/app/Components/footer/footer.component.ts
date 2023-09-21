eb5504863fffdcdb14065d85bb72b8bd9176b1b4
Jubril Muritala <jubrilmuritala69@gmail.com>
2023-09-21 14:09:53 +0100

    partner link added

--- src/app/Components/footer/footer.component.ts
+++ src/app/Components/footer/footer.component.ts
@@ -3,22 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   fb() {
-    window.open('https://www.facebook.com/HalogenNGR', '_blank')
+    window.open('https://www.facebook.com/HalogenNGR', '_blank');
   }
   insta() {
     // window.location.href = "https://www.instagram.com/halogen_group/"
     window.open('https://www.instagram.com/halogen_group', '_blank');
   }
   toElem() {
-    window.location.href = 'https://halogen-group.com/new-website/#elem'
+    window.location.href = 'https://halogen-group.com/new-website/#elem';
+  }
+  toPartner() {
+    window.open('https://partner.halobizapps.com/#/', '_blank');
   }
 }
