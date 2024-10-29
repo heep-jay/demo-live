cda1c43a9490b3be2cc6dfdafb197c489ffda6a6
unknown <jubrilmuritala69@gmail.com>
2024-10-29 08:34:29 +0100

    Website fixes

--- src/app/Components/leaders/leaders.component.ts
+++ src/app/Components/leaders/leaders.component.ts
@@ -1,19 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  inject,
+  TemplateRef,
+  ViewEncapsulation,
+  Input,
+  OnInit,
+} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-leaders',
   templateUrl: './leaders.component.html',
-  styleUrls: ['./leaders.component.css']
+  styleUrls: ['./leaders.component.css'],
 })
 export class LeadersComponent implements OnInit {
-  @Input() leader: any
-  constructor(public dialog: MatDialog) { }
+  @Input() leader: any;
+  private modalService = inject(NgbModal);
+  constructor(public dialog: MatDialog) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   openDialog(content: any) {
     this.dialog.open(content);
   }
 
+  openScrollableContent(longContent: any) {
+    this.modalService.open(longContent, { scrollable: true, size: 'lg' });
+  }
 }
